31d28fb6606fae26aa8b748b54a903f89d1d9c66
160856 <160856@stud.prz.edu.pl>
2021-04-15 19:54:35 +0200

    Ukonczone

--- temp.py
+++ temp.py
@@ -22,33 +22,29 @@ def GenerujTabliceGeometrii(x_0,x_p,n):
     val = (x_p-x_0)/(n-1)
     tablica_w = np.array([x_0])
 
-    for indeks_tab1 in range(1,n,1):
-        tablica_w = np.block([tablica_w, indeks_tab1 * val + x_0])
+    for indeks1 in range(1,n,1):
+        tablica_w = np.block([tablica_w, indeks1* val + x_0])
 
     tablica_e = np.zeros((n-1,2))
 
-    for indeks_tab2 in range(0, n-1, 1):
-        tablica_e[indeks_tab2][0] = indeks_tab2+1
-        tablica_e[indeks_tab2][1] = indeks_tab2+2
+    for indeks2 in range(0, n-1, 1):
+        tablica_e[indeks2][0] = indeks2+1
+        tablica_e[indeks2][1] = indeks2+2
 
 
-    return indeks_tab1,tablica_w,tablica_e
+    return indeks1,tablica_w,tablica_e
 
 
 def Rysuj_geometrie(tablica_w,tablica_e, n):
     tab=np.zeros((n))
     l1 = plt.plot(tablica_w,tab)
-    for i in range(0,n,1):
-        plt.text(tablica_w[i], -0.01, str(i+1))
-        plt.text(tablica_w[i], 0, "|")
-        plt.text(tablica_w[i+1]/2+tablica_w[i]/2, 0.005 ,str(i+1))
+    for indeks in range(0,n,1):
+        plt.text(tablica_w[indeks], -0.01, str(i+1))
+        plt.text(tablica_w[indeks], 0, "|")
+        plt.text(tablica_w[indeks+1]/2+tablica_w[indeks]/2, 0.005 ,str(indeks+1))
 
 
 
 
 Wezly, Tablica1, Tablica2 = GenerujTabliceGeometrii(0,1,4)
-Liczba_wezlow = Wezly + 1
-print('Liczba wezlow: ', Liczba_wezlow)
-print('Tablica wezlow: ', Tablica1)
-print('Tablica elementow: ', Tablica2)
 Rysuj_geometrie(Tablica1, Tablica2, Wezly+1)
